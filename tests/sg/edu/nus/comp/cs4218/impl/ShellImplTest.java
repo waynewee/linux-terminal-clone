674790d4c1d2ab66a9a2af39e502338cf0f6ab0f
Jing Wei <jingwei.chew@gmail.com>
2021-02-07 14:38:30 +0800

    add test structure

---
+++ tests/sg/edu/nus/comp/cs4218/impl/ShellImplTest.java
@@ -0,0 +1,10 @@
+package sg.edu.nus.comp.cs4218.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShellImplTest {
+
+    @org.junit.jupiter.api.Test
+    void parseAndEvaluate() {
+    }
+}
