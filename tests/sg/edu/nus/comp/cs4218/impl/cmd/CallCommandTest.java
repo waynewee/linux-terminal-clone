7a24ba797ff27ac449ed24c456063a6efdd68c8b
Jaivignesh Venugopal <smugs1995@gmail.com>
2021-02-22 15:18:21 +0800

    update tests for call command

--- tests/sg/edu/nus/comp/cs4218/impl/cmd/CallCommandTest.java
+++ tests/sg/edu/nus/comp/cs4218/impl/cmd/CallCommandTest.java
@@ -1,35 +1,62 @@
 package sg.edu.nus.comp.cs4218.impl.cmd;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import sg.edu.nus.comp.cs4218.Environment;
 import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
 import sg.edu.nus.comp.cs4218.exception.ShellException;
+import sg.edu.nus.comp.cs4218.impl.ShellImpl;
 import sg.edu.nus.comp.cs4218.impl.util.ApplicationRunner;
 import sg.edu.nus.comp.cs4218.impl.util.ArgumentResolver;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CallCommandTest {
 
+    private static ByteArrayOutputStream outputStream;
+    private static List<String> tokens;
+
+    @BeforeEach
+    // Prepare output stream
+    public void prepareOutputStream() {
+        outputStream = new ByteArrayOutputStream();
+    }
+    
+    @BeforeEach
+    // Prepare list of tokens
+    public void prepareTokensList() {
+        tokens = new LinkedList<>();
+    }
+    
     @Test
     public void evaluate_SimpleCommand_ReturnsOutput() throws AbstractApplicationException, ShellException {
-        // Prepare empty list of tokens
-        List<String> tokens = new LinkedList<>();
+        // Add 'ls' command as token
         tokens.add("ls");
+
         // Initialise callCommand
         CallCommand callCommand = new CallCommand(tokens, new ApplicationRunner(), new ArgumentResolver());
-        // Prepare output stream
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 
+        // Prepare correct output
+        boolean correctOutput;
+        correctOutput = Objects.requireNonNull(new File(Environment.currentDirectory).listFiles()).length == 0;
 
         callCommand.evaluate(System.in, outputStream);
-        System.out.println(outputStream.toString());
-        assertFalse(outputStream.toString().isEmpty());
+        assertEquals(correctOutput, outputStream.toString().isEmpty());
     }
+    
+    @Test 
+    public void getArgsList_ReturnsNonNullInstance() {
+        // Initialise callCommand
+        CallCommand callCommand = new CallCommand(tokens, new ApplicationRunner(), new ArgumentResolver());
 
+        assert (callCommand.getArgsList() != null);
+    }
 
 }
