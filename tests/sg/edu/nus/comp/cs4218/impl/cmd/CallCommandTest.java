39fe909fdfe27f1bab5a1c275c8079441fdd9e58
Jaivignesh Venugopal <smugs1995@gmail.com>
2021-02-22 16:37:09 +0800

    update call command tests

--- tests/sg/edu/nus/comp/cs4218/impl/cmd/CallCommandTest.java
+++ tests/sg/edu/nus/comp/cs4218/impl/cmd/CallCommandTest.java
@@ -1,12 +1,10 @@
 package sg.edu.nus.comp.cs4218.impl.cmd;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import sg.edu.nus.comp.cs4218.Environment;
 import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
 import sg.edu.nus.comp.cs4218.exception.ShellException;
-import sg.edu.nus.comp.cs4218.impl.ShellImpl;
 import sg.edu.nus.comp.cs4218.impl.util.ApplicationRunner;
 import sg.edu.nus.comp.cs4218.impl.util.ArgumentResolver;
 
@@ -17,6 +15,8 @@ import java.util.List;
 import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_INVALID_APP;
+import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_SYNTAX;
 
 class CallCommandTest {
 
@@ -50,13 +50,45 @@ class CallCommandTest {
         callCommand.evaluate(System.in, outputStream);
         assertEquals(correctOutput, outputStream.toString().isEmpty());
     }
-    
-    @Test 
-    public void getArgsList_ReturnsNonNullInstance() {
+
+    @Test
+    public void evaluate_EmptyCommand_ThrowsShellException() {
         // Initialise callCommand
         CallCommand callCommand = new CallCommand(tokens, new ApplicationRunner(), new ArgumentResolver());
+        // Assert right exception thrown
+        ShellException shellException = assertThrows(ShellException.class, () -> callCommand.evaluate(System.in, outputStream));
+        // Assert right message in exception
+        assertEquals(new ShellException(ERR_SYNTAX).getMessage(), shellException.getMessage());
+    }
 
-        assert (callCommand.getArgsList() != null);
+    @Test
+    public void evaluate_UnknownCommand_ThrowsShellException() {
+        // Add unimplemented command as token
+        tokens.add("move");
+        tokens.add("file.txt");
+        tokens.add("new_file.txt");
+
+        // Initialise callCommand
+        CallCommand callCommand = new CallCommand(tokens, new ApplicationRunner(), new ArgumentResolver());
+        // Assert right exception thrown
+        ShellException shellException = assertThrows(ShellException.class, () -> callCommand.evaluate(System.in, outputStream));
+        // Assert right message in exception
+        assertEquals(new ShellException(tokens.get(0) + ": " + ERR_INVALID_APP).getMessage(), shellException.getMessage());
+    }
+
+    @Test 
+    public void getArgsList_ReturnsListInstance() {
+        // Add unimplemented command as token
+        tokens.add("command1");
+        tokens.add("command2");
+        tokens.add("command3");
+
+        // Initialise callCommand
+        CallCommand callCommand = new CallCommand(tokens, new ApplicationRunner(), new ArgumentResolver());
+        // Assert return value is not null
+        assert(callCommand.getArgsList() != null);
+        // Assert object is instance of List
+        assertTrue(callCommand.getArgsList() instanceof List);
     }
 
 }
