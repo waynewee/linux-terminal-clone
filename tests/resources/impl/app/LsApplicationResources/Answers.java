00b716fa750bdf518c5dda5e9c559ab25301b6be
Jaivignesh Venugopal <smugs1995@gmail.com>
2021-02-25 23:39:29 +0800

    add resource files

---
+++ tests/resources/impl/app/LsApplicationResources/Answers.java
@@ -0,0 +1,26 @@
+package resources.impl.app.LsApplicationResources;
+
+public class Answers {
+    public static String testRecursive =
+            "tests\\resources\\impl\\app\\LsApplicationResources\\test_recursive:\n" +
+            "answer.txt\n" +
+            "vig1\n" +
+            "vig2\n" +
+            "vig3\n" +
+            "\n" +
+            "tests\\resources\\impl\\app\\LsApplicationResources\\test_recursive\\vig1:\n" +
+            "vig1doc1.txt\n" +
+            "vig1doc2.txt\n" +
+            "vig1doc3.txt\n" +
+            "vig1vig1\n" +
+            "\n" +
+            "tests\\resources\\impl\\app\\LsApplicationResources\\test_recursive\\vig1\\vig1vig1:\n" +
+            "vig1vig1doc1.txt\n" +
+            "\n" +
+            "tests\\resources\\impl\\app\\LsApplicationResources\\test_recursive\\vig2:\n" +
+            "vig2doc1.txt\n" +
+            "vig2doc2.txt\n" +
+            "\n" +
+            "tests\\resources\\impl\\app\\LsApplicationResources\\test_recursive\\vig3:\n" +
+            "vig3doc1.txt\n";
+}
