40e949424fc3517719de0e7fc4ad2cf24d6e8936
Jing Wei <jingwei.chew@gmail.com>
2021-02-28 18:15:57 +0800

    Add more unit tests for parseCommand and semiColon

--- tests/sg/edu/nus/comp/cs4218/unimpl/util/CommandBuilderTestUnimplemented.java
+++ tests/sg/edu/nus/comp/cs4218/unimpl/util/CommandBuilderTestUnimplemented.java
@@ -15,12 +15,30 @@ import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_NOT_SUPPORTED;
 
 class CommandBuilderTestUnimplemented {
     @Test
-    public void parseCommand_backquotedSubCommand_isAnArgument() throws ShellException {
+    public void parseCommand_backquotedSubCommand_isOneArgument() throws ShellException {
         Command command = CommandBuilder.parseCommand("echo `echo hi`", new ApplicationRunner());
         CallCommand callCommand = (CallCommand) command;
         List<String> argsList = callCommand.getArgsList();
         assertEquals("`echo hi`", argsList.get(1));
     }
+
+    @Test
+    public void parseCommand_backquotedSubCommandWithQuotes_isOneArgument() throws ShellException {
+        Command command = CommandBuilder.parseCommand("echo `echo \"`this is not special`\"`", new ApplicationRunner());
+        CallCommand callCommand = (CallCommand) command;
+        List<String> argsList = callCommand.getArgsList();
+        assertEquals("`echo \"`this is not special`\"`", argsList.get(1));
+    }
+
+    @Test
+    /**
+     * In typical UNIX systems, a new line will be prompted but our shell does not support multiline commands
+     */
+    public void parseCommand_incompleteBackquote_throwsShellException() throws ShellException {
+        assertThrows(ShellException.class, () ->
+                CommandBuilder.parseCommand("`echo hi", new ApplicationRunner()));
+    }
+
 
     @Test
     public void parseCommand_oneSemiColon_makesTwoCommands() throws ShellException {
@@ -34,6 +52,13 @@ class CommandBuilderTestUnimplemented {
         Command command = CommandBuilder.parseCommand("echo hi;", new ApplicationRunner());
         SequenceCommand sequenceCommand = (SequenceCommand) command;
         assertEquals(1, sequenceCommand.getCommands().size());
+    }
+
+    @Test
+    public void parseCommand_semiColonInSubcommand_makesOneCommand() throws ShellException {
+        Command command = CommandBuilder.parseCommand("echo `echo hi; echo bye`", new ApplicationRunner());
+        CallCommand callCommand = (CallCommand) command;
+        assertEquals(2, callCommand.getArgsList().size());
     }
 
     @Test
