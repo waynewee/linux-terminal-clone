f514b42522d12a95a506f785c74774c74cd99317
Phoenix <terenceng1996@gmail.com>
2021-02-28 16:35:44 +0800

    Modified folder structure

--- src/main/java/sg/edu/nus/comp/cs4218/exception/ExitException.java
+++ src/main/java/sg/edu/nus/comp/cs4218/exception/ExitException.java
@@ -1,6 +1,7 @@
 package sg.edu.nus.comp.cs4218.exception;
 
 public class ExitException extends AbstractApplicationException {
+    
     private static final long serialVersionUID = 6517503252362314995L;
 
     /**
--- src/main/java/sg/edu/nus/comp/cs4218/exception/InvalidArgsException.java
+++ src/main/java/sg/edu/nus/comp/cs4218/exception/InvalidArgsException.java
@@ -1,6 +1,9 @@
 package sg.edu.nus.comp.cs4218.exception;
 
 public class InvalidArgsException extends Exception {
+
+    private static final long serialVersionUID = 7060671468402158735L;
+
     public InvalidArgsException(String message) {
         super(message);
     }
