544fefef691807558fa71aa7fc1c90b7420c0e94
Jaivignesh Venugopal <smugs1995@gmail.com>
2021-03-21 09:40:26 +0800

    rename all occurences with my name

--- src/test/java/sg/edu/nus/comp/cs4218/impl/app/LsApplicationTest.java
+++ src/test/java/sg/edu/nus/comp/cs4218/impl/app/LsApplicationTest.java
@@ -33,66 +33,66 @@ class LsApplicationTest {
     public static String testRecursive =
             "src\\test\\resources\\impl\\app\\LsApplicationResources\\test_recursive:\n" +
                     "answer.txt\n" +
-                    "vig1\n" +
-                    "vig2\n" +
-                    "vig3\n" +
+                    "space1\n" +
+                    "space2\n" +
+                    "space3\n" +
                     "\n" +
-                    "src\\test\\resources\\impl\\app\\LsApplicationResources\\test_recursive\\vig1:\n" +
-                    "vig1doc1.txt\n" +
-                    "vig1doc2.txt\n" +
-                    "vig1doc3.txt\n" +
-                    "vig1vig1\n" +
+                    "src\\test\\resources\\impl\\app\\LsApplicationResources\\test_recursive\\space1:\n" +
+                    "space1doc1.txt\n" +
+                    "space1doc2.txt\n" +
+                    "space1doc3.txt\n" +
+                    "space1space1\n" +
                     "\n" +
-                    "src\\test\\resources\\impl\\app\\LsApplicationResources\\test_recursive\\vig1\\vig1vig1:\n" +
-                    "vig1vig1doc1.txt\n" +
+                    "src\\test\\resources\\impl\\app\\LsApplicationResources\\test_recursive\\space1\\space1space1:\n" +
+                    "space1space1doc1.txt\n" +
                     "\n" +
-                    "src\\test\\resources\\impl\\app\\LsApplicationResources\\test_recursive\\vig2:\n" +
-                    "vig2doc1.txt\n" +
-                    "vig2doc2.txt\n" +
+                    "src\\test\\resources\\impl\\app\\LsApplicationResources\\test_recursive\\space2:\n" +
+                    "space2doc1.txt\n" +
+                    "space2doc2.txt\n" +
                     "\n" +
-                    "src\\test\\resources\\impl\\app\\LsApplicationResources\\test_recursive\\vig3:\n" +
-                    "vig3doc1.txt\n";
+                    "src\\test\\resources\\impl\\app\\LsApplicationResources\\test_recursive\\space3:\n" +
+                    "space3doc1.txt\n";
 
     public static String testRecursiveDirectories =
             "src\\test\\resources\\impl\\app\\LsApplicationResources\\test_recursive_directories:\n" +
-                    "vig1\n" +
-                    "vig2\n" +
-                    "vig3\n" +
+                    "space1\n" +
+                    "space2\n" +
+                    "space3\n" +
                     "\n" +
-                    "src\\test\\resources\\impl\\app\\LsApplicationResources\\test_recursive_directories\\vig3:\n" +
-                    "vig3vig1\n";
+                    "src\\test\\resources\\impl\\app\\LsApplicationResources\\test_recursive_directories\\space3:\n" +
+                    "space3space1\n";
 
 
     public static String testRecursiveSort =
             "src\\test\\resources\\impl\\app\\LsApplicationResources\\test_recursive_sort:\n" +
-                    "vig1\n" +
-                    "vig2\n" +
-                    "vig3\n" +
+                    "space1\n" +
+                    "space2\n" +
+                    "space3\n" +
                     "answer.txt\n" +
                     "\n" +
-                    "src\\test\\resources\\impl\\app\\LsApplicationResources\\test_recursive_sort\\vig1:\n" +
-                    "vig1vig1\n" +
+                    "src\\test\\resources\\impl\\app\\LsApplicationResources\\test_recursive_sort\\space1:\n" +
+                    "space1space1\n" +
                     "test3.doc\n" +
-                    "vig1doc1.txt\n" +
-                    "vig1doc3.txt\n" +
+                    "space1doc1.txt\n" +
+                    "space1doc3.txt\n" +
                     "test.xls\n" +
                     "\n" +
-                    "src\\test\\resources\\impl\\app\\LsApplicationResources\\test_recursive_sort\\vig1\\vig1vig1:\n" +
-                    "vig1vig1doc1.txt\n" +
-                    "vig1vig2doc3.txt\n" +
-                    "vig2vig1doc2.txt\n" +
+                    "src\\test\\resources\\impl\\app\\LsApplicationResources\\test_recursive_sort\\space1\\space1space1:\n" +
+                    "space1space1doc1.txt\n" +
+                    "space1space2doc3.txt\n" +
+                    "space2space1doc2.txt\n" +
                     "\n" +
-                    "src\\test\\resources\\impl\\app\\LsApplicationResources\\test_recursive_sort\\vig2:\n" +
+                    "src\\test\\resources\\impl\\app\\LsApplicationResources\\test_recursive_sort\\space2:\n" +
                     "newppt.pptx\n" +
                     "newxls.xls\n" +
                     "\n" +
-                    "src\\test\\resources\\impl\\app\\LsApplicationResources\\test_recursive_sort\\vig3:\n" +
-                    "vig3vig1\n" +
-                    "vig3doc1.txt.py.txt\n" +
+                    "src\\test\\resources\\impl\\app\\LsApplicationResources\\test_recursive_sort\\space3:\n" +
+                    "space3space1\n" +
+                    "space3doc1.txt.py.txt\n" +
                     "\n" +
-                    "src\\test\\resources\\impl\\app\\LsApplicationResources\\test_recursive_sort\\vig3\\vig3vig1:\n" +
-                    "vig1vig1doc1.txt\n" +
-                    "vig3vig5vig6.txt\n";
+                    "src\\test\\resources\\impl\\app\\LsApplicationResources\\test_recursive_sort\\space3\\space3space1:\n" +
+                    "space1space1doc1.txt\n" +
+                    "space3space5space6.txt\n";
 
     public static String testFoldersOnlySort =
             "src\\test\\resources\\impl\\app\\LsApplicationResources\\test_folders_sort:\n" +
